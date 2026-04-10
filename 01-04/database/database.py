a664e71a6bd3392d9c2f2c3041c36e25585e1a48
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-04-10 08:27:56 -0300

    refactor: removido arquivos não utilizados.

--- 01-04/database/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models.models import Base
-
-engine = create_engine("sqlite:///biblioteca.db")
-
-SessionaLocal = sessionmaker(bind=engine)
-
-Base.metadata.create_all(engine)
--- 01-04/src/exceptions/exceptions.py
+++
@@ -1,6 +0,0 @@
-'''Exceções Customizadas utilizadas na API'''
-
-class ValueErrorISBN(Exception):
-    '''Exceção representativa para atributo ISBN'''
-    def __init__(self, isbn):
-        super().__init__(f'O isbn {isbn} não possui um tamanho válido para 13 caracteres.')
