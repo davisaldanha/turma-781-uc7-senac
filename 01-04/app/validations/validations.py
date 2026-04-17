409e7d9e57b89863c45d2e7806b9f1c25ac7d9f1
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-04-17 15:29:45 -0300

    feat: corrigir importação de exceções e adicionar validações de senha

--- 01-04/app/validations/validations.py
+++ 01-04/app/validations/validations.py
@@ -1,6 +1,6 @@
 '''Funções para validações de dados'''
 
-from exceptions.exceptions import *
+from app.exceptions.exceptions import *
 
 def valid_isbn(isbn: str):
     '''
@@ -11,3 +11,14 @@ def valid_isbn(isbn: str):
     '''
     if not len(isbn) == 13:
         raise ValueErrorISBN(isbn)
+
+def valid_password(password: str):
+    '''
+    Retorna uma exec. `PasswordLengthError` caso a senha não contenha no mínimo 8 caracteres.
+
+    Args:
+        password (str): senha do usuário
+    
+    '''
+    if len(password) < 8:
+        raise PasswordLengthError(password)
