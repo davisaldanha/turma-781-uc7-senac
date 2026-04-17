bee2acd267ab2dec212c6824f82f1a9a28854c8b
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-04-17 15:29:59 -0300

    feat: adicionar exceções personalizadas para validação de usuário e senha

--- 01-04/app/exceptions/exceptions.py
+++ 01-04/app/exceptions/exceptions.py
@@ -4,3 +4,13 @@ class ValueErrorISBN(Exception):
     '''Exceção representativa para atributo ISBN'''
     def __init__(self, isbn):
         super().__init__(f'O isbn {isbn} não possui um tamanho válido para 13 caracteres.')
+
+class UserNotFoundError(Exception):
+    ''''Exceção representativa para usuário não encontrado'''
+    def __init__(self, username):
+        super().__init__(f'O usuário {username} não foi encontrado.')
+
+class PasswordLengthError(Exception):
+    '''Exceção representativa para senha com comprimento inválido'''
+    def __init__(self, password):
+        super().__init__(f'A senha {password} deve conter no mínimo 8 caracteres.')
